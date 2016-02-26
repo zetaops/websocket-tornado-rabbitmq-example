d8cbce54a0dc0e76d12607bfb9920ff0b368739e
Evren Esat Özkan <evrenesat@zetaops.io>
2016-02-26 15:06:23 +0200

    First commit

---
+++ queue.py
@@ -0,0 +1,101 @@
+# -*-  coding: utf-8 -*-
+import logging
+import pika
+import time
+from pika.adapters import TornadoConnection
+pika.log = logging.getLogger(__name__)
+
+
+class PikaClient(object):
+    INPUT_QUEUE_NAME = 'in_queue'
+    def __init__(self, io_loop):
+        self.io_loop = io_loop
+        self.received_message_counter = 0
+        self.sent_message_counter = 0
+        self.start_time = -1
+        self.connected = False
+        self.connecting = False
+        self.connection = None
+        self.in_channel = None
+        self.out_channels = {}
+        self.websockets = {}
+        self.connect()
+
+    def connect(self):
+        if self.connecting:
+            return
+        self.connecting = True
+
+        cred = pika.PlainCredentials('guest', 'guest')
+        param = pika.ConnectionParameters(
+            host='localhost',
+            port=5672,
+            virtual_host='/',
+            credentials=cred
+        )
+
+        self.connection = TornadoConnection(param,
+                                            on_open_callback=self.on_connected)
+
+    def on_connected(self, connection):
+        self.connected = True
+        self.connection = connection
+        self.in_channel = self.connection.channel(self.on_conn_open)
+
+    def on_conn_open(self, channel):
+        self.in_channel.exchange_declare(exchange='tornado_input', type='topic')
+        channel.queue_declare(callback=self.on_input_queue_declare, queue=self.INPUT_QUEUE_NAME)
+
+    def on_input_queue_declare(self, queue):
+        self.in_channel.queue_bind(callback=None,
+                           exchange='tornado_input',
+                           queue=self.INPUT_QUEUE_NAME,
+                           routing_key="#")
+
+    def register_websocket(self, sess_id, ws):
+        self.websockets[sess_id] = ws
+        channel = self.create_out_channel(sess_id)
+
+
+    def unregister_websocket(self, sess_id):
+        del self.websockets[sess_id]
+        if sess_id in self.out_channels:
+            self.out_channels[sess_id].close()
+        print("Time: %s, In: %s Out: %s" % (int(time.time() - self.start_time),
+                                                  self.received_message_counter,
+                                                  self.sent_message_counter) )
+
+
+    def create_out_channel(self, sess_id):
+        def on_output_channel_creation(channel):
+            def on_output_queue_decleration(queue):
+                channel.basic_consume(self.on_message, queue=sess_id)
+            self.out_channels[sess_id] = channel
+            channel.queue_declare(callback=on_output_queue_decleration,
+                                  queue=sess_id,
+                                  auto_delete=True,
+                                  exclusive=True)
+
+        self.connection.channel(on_output_channel_creation)
+
+
+    def redirect_incoming_message(self, sess_id, message):
+        if not self.sent_message_counter:
+            self.start_time = time.time()
+        self.received_message_counter += 1
+        if not self.received_message_counter % 1000:
+            print("Total Received: %s " % self.received_message_counter)
+        self.in_channel.basic_publish(exchange='tornado_input',
+                              routing_key='in.%s' % sess_id,
+                              body=message)
+
+    def on_message(self, channel, method, header, body):
+        self.sent_message_counter += 1
+        sess_id = method.routing_key
+        if sess_id in self.websockets:
+            self.websockets[sess_id].write_message(body)
+            channel.basic_ack(delivery_tag=method.delivery_tag)
+        else:
+            channel.basic_reject(delivery_tag=method.delivery_tag)
+
+
---
+++ server.py
@@ -0,0 +1,70 @@
+# -*-  coding: utf-8 -*-
+"""
+tornado websocket proxy for WF worker daemons
+"""
+
+
+from queue import PikaClient
+from uuid import uuid4
+
+from tornado import websocket, web, ioloop
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def _get_sess_id(self):
+        return self.sess_id;
+
+    def open(self):
+        self.sess_id = uuid4().hex
+        self.application.pc.register_websocket(self._get_sess_id(), self)
+
+    def on_message(self, message):
+        self.application.pc.redirect_incoming_message(self._get_sess_id(), message)
+
+    def on_close(self):
+        """
+        remove connection from pool on connection close.
+        """
+        self.application.pc.unregister_websocket(self._get_sess_id())
+
+class Connect(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, *args):
+        with open('connector.html') as fh:
+            self.write(fh.read())
+            self.finish()
+
+class Index(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, *args):
+        with open('index.html') as fh:
+            self.write(fh.read())
+            self.finish()
+
+app = web.Application([
+    (r'/ws', SocketHandler),
+    (r'/connect', Connect),
+    (r'/', Index),
+])
+
+def runserver():
+    my_ioloop = ioloop.IOLoop.instance()
+
+    # setup pika client
+    pc = PikaClient(my_ioloop)
+    app.pc = pc
+    pc.connect()
+    app.listen(9001)
+    my_ioloop.start()
+
+if __name__ == '__main__':
+    runserver()
+
